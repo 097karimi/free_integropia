a26077d8d99a9302550a247a1587f991b6e8ede1
Amir <097karimi@gmail.com>
2022-11-09 21:44:54 +0330

    fix exception handelling

--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ from django.db import models
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
-
+from django.core.exceptions import *
 from django.db import connection
 
 
@@ -20,19 +20,16 @@ def my_custom_sql(sql):
 
 
 def api(request):
-    header, data = list(my_custom_sql(Query.objects.get(pk=1).query))
+    try:
+        header, data = list(my_custom_sql(Query.objects.get().query))
+    except:
+        header, data = [], []
     return JsonResponse({'data': data, 'header': header})
 
 
-# class api(APIView):
-#     def get(self, request):
-#         """
-#         Return a list of all users.
-#         """
-#         names = [e.name for e in Student.objects.raw(Query.objects.get(pk=1).query)]
-#         return jsonResponse(names)
-
-
 def home(request):
-    header, data = list(my_custom_sql(Query.objects.get(pk=1).query))
+    try:
+        header, data = list(my_custom_sql(Query.objects.get().query))
+    except:
+        header, data = [], []
     return render(request, 'index.html', {'data': data, 'header': header})
